0a23b97d2d0721ea86ad6f97ca14700075146361
Parma <User@parma>
2019-06-27 10:32:38 +0530

    changes in the project page

--- project-manager-ui/src/app/add-project/project.component.ts
+++ project-manager-ui/src/app/add-project/project.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import { ProjectModel } from '../model/project.model';
 import { AppService } from '../service/app.service';
-
+import { DatePipe } from '@angular/common';
 
 @Component({
     selector:'project',
@@ -11,6 +11,8 @@ import { AppService } from '../service/app.service';
 export class ProjectComponent implements OnInit{
     project : ProjectModel;
     projectList : ProjectModel[];
+    checkboxValue: boolean;
+    datePipe: DatePipe = new DatePipe("en-US");
 
     ngOnInit(){
         this.getAllProject();
@@ -20,12 +22,24 @@ export class ProjectComponent implements OnInit{
         this._appService.getProjects()
         .subscribe(res => {
         this.projectList = res.outData;
-        console.log("from add User component:"+this.projectList);
+        console.log("from add Porject component:"+this.projectList);
         });
     }
     
     constructor(private _appService: AppService) { 
         this.project = new ProjectModel();    
+    }
+
+    public setStartAndEndDate():void{        
+        if (this.checkboxValue) {            
+            let nextDate = new Date();
+            nextDate.setDate(nextDate.getDate() + 1);            
+            this.project.startDate = this.datePipe.transform(new Date(), 'yyyy-MM-dd');
+            this.project.endDate = this.datePipe.transform(nextDate, 'yyyy-MM-dd');            
+          } else {
+            this.project.startDate = "";
+            this.project.endDate = "";
+          }
     }
 
     public saveOrUpdateProject() : void{        
